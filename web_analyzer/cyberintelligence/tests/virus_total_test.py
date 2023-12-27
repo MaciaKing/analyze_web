51b4aadba11e838de57abfd500b874f584ddafc1
MaciaKing <maciasalvasalva@gmail.com>
2023-12-27 20:01:39 +0100

    [feat]: improve virus_total.py and add test

---
+++ web_analyzer/cyberintelligence/tests/virus_total_test.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from cyberintelligence.virus_total import VirusTotal
+from django.conf import settings
+
+class VirusTotalTestCase(TestCase):
+    def setUp(self):
+        self.vt = VirusTotal(settings.VIRUS_TOTAL_API_KEY) # better create another api key
+    
+    def test_make_ip_query(self):
+        self.assertTrue(isinstance(self.vt.make_ip_query('8.8.8.8'), dict), "'response' is a dict class")
+        self.assertRaises(Exception, lambda: self.vt.make_ip_query('google.com'))
+
+    def test_make_domain_query(self):
+        self.assertTrue(isinstance(self.vt.make_domain_query('google.com'), dict), "'response' is a dict class")
+        self.assertRaises(Exception, lambda: self.vt.make_domain_query('8.8.8.8'))
--- web_analyzer/cyberintelligence/virus_total.py
+++ web_analyzer/cyberintelligence/virus_total.py
@@ -1,20 +1,32 @@
 import requests
+import json
 
 class VirusTotal():
+    # 500 requests per day and a rate of 4 requests per minute. Free key
+    
     def __init__(self, api_key):
         self.api_key = api_key
         self.base_url="https://www.virustotal.com/api/v3/"
         self.base_ip_address_endpoint = "ip_addresses/"
+        self.base_domain_endpoint = "domains/"
         self.header = {
             "x-apikey": self.api_key
         }
 
     def make_ip_query(self, ip):
-        # 500 requests per day and a rate of 4 requests per minute. Free key
         url = self.base_url + self.base_ip_address_endpoint + ip
-        print(url)
         response = requests.get(url, headers=self.header)
-        print(response.text)
-        #return response
-    
+        if response.status_code == 200:
+            return json.loads(response.text)
+        else:
+            raise Exception("An ip was expected")     
+        
+    def make_domain_query(self, domain):
+        url = self.base_url + self.base_domain_endpoint + domain
+        response = requests.get(url, headers=self.header)
+        if response.status_code == 200:
+            return json.loads(response.text)
+        else:
+            raise Exception("A domain was expected")
+
         
