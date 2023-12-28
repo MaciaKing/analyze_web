2821a268bd3cc2060a2fd7ca82d0dc4f8cfb86c3
MaciaKing <maciasalvasalva@gmail.com>
2023-12-28 11:13:14 +0100

    [feat]: add make_ip_query and make_domain_query works. Test code is missing

--- web_analyzer/cyberintelligence/classes/alien_vault.py
+++ web_analyzer/cyberintelligence/classes/alien_vault.py
@@ -6,7 +6,7 @@ class AlienVault():
 
     def __init__(self, api_key):
         self.api_key = api_key
-        self.base_url = "https://otx.alienvault.com/api/v1/"
+        self.base_url = 'https://otx.alienvault.com/api/v1/'
         self.header = {
             'X-OTX-API-KEY': self.api_key,
             'Content-Type': 'application/json'
@@ -14,4 +14,8 @@ class AlienVault():
     
     def make_ip_query(self, ip):
         response = requests.post(self.base_url + f"indicators/IPv4/{ip}/general", headers=self.header, data='')
-        return response.text
+        return json.loads(response.text)
+    
+    def make_domain_query(self, domain):
+        response = requests.get(self.base_url + f"indicators/domain/{domain}/general", headers=self.header, data='')
+        return json.loads(response.text)
