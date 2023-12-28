1276e3c5468ad73575670cced9cf6f9b6b45e2b6
MaciaKing <maciasalvasalva@gmail.com>
2023-12-28 21:40:59 +0100

    [feat]: add comments to the code

--- web_analyzer/cyberintelligence/classes/helper.py
+++ web_analyzer/cyberintelligence/classes/helper.py
@@ -5,6 +5,20 @@ class Helper:
 
     @staticmethod
     def make_query(url, header):
+        """ Make a query to the url
+        Params:
+            - url (string). This is the url that will be requested.
+            - header (dict). This is the header to send to the request.
+
+        Functionality:
+            - Make a query GET query to the url. 
+            If status_code is 200,return the text request. 
+            If status_cose is diferent, raise an Exception.
+        
+        Returns:
+            - Return a 'dict' (json) object.
+            - Returns an error because the url is malformed.
+        """
         response = requests.get(url, headers=header)
         if response.status_code == 200:
             return json.loads(response.text)
--- web_analyzer/cyberintelligence/classes/virus_total.py
+++ web_analyzer/cyberintelligence/classes/virus_total.py
@@ -1,7 +1,13 @@
 from cyberintelligence.classes.helper import Helper
 
 class VirusTotal():
-    # 500 requests per day and a rate of 4 requests per minute. Free key
+    """ This class is to connect my code to virus total api.
+    For more information on the total virus api visit
+    this link: https://virustotal.readme.io/reference/overview
+
+    If you are using the free token it only allows you to make 
+    500 requests per day and a rate of 4 requests per minute.
+    """
     def __init__(self, api_key):
         self.api_key = api_key
         self.base_url="https://www.virustotal.com/api/v3/"
@@ -12,7 +18,27 @@ class VirusTotal():
         }
 
     def make_ip_query(self, ip):
+        """ Make an ip query to Virus Total api.
+        Params:
+            - ip (string). Represents the IP to be analyzed.
+
+        Functionality:
+            - Creates a valid request for the total virus endpoint.
+        
+        Returns:
+            - Return a 'dict' (json) object.
+        """
         return Helper.make_query(self.base_url + self.base_ip_address_endpoint + ip, self.header)
         
     def make_domain_query(self, domain):
+        """ Make a domain query to Virus Total api.
+        Params:
+            - domain (string). Represents the domain to be analyzed.
+
+        Functionality:
+            - Creates a valid request for the total virus endpoint.
+        
+        Returns:
+            - Return a 'dict' (json) object.
+        """
         return Helper.make_query(self.base_url + self.base_domain_endpoint + domain, self.header)
