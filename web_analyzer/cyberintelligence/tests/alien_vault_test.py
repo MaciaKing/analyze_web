d2e1a883833945c23dad778132d347961f5469c5
MaciaKing <maciasalvasalva@gmail.com>
2023-12-28 20:56:27 +0100

    [feat]: finish test for alien_vault.py

--- web_analyzer/cyberintelligence/tests/alien_vault_test.py
+++ web_analyzer/cyberintelligence/tests/alien_vault_test.py
@@ -7,4 +7,9 @@ class AlienVaultTestCase(TestCase):
         self.av = AlienVault(settings.ALIEN_VAULT_API_KEY)
 
     def test_make_ip_query(self):
-        self.assertTrue(isinstance(self.av.make_ip_query('google.com'), dict), "'response' is a dict class")
+        self.assertTrue(isinstance(self.av.make_ip_query('8.8.8.8'), dict), "'response' is a dict class")
+        self.assertRaises(Exception, lambda: self.av.make_ip_query('google.com'))
+
+    def test_make_domain_query(self):
+        self.assertTrue(isinstance(self.av.make_domain_query('google.com'), dict), "'response' is a dict class")
+        self.assertRaises(Exception, lambda: self.av.make_domain_query('8.8.8.8'))
