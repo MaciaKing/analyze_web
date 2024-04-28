fe60e0a6af5730b51e4a81563943d086a4b22e02
MaciaKing <maciasalvasalva@gmail.com>
2024-04-28 21:27:32 +0200

    [fix]: add all __init__.py files

---
+++ cyberintelligence/models/__init__.py
@@ -0,0 +1,4 @@
+# Include the models that do you want to migrate it
+from .last_line_read import LastLineRead
+from .data_extracted import DataExtracted
+from .pulsedive import Pulsedive
---
+++ cyberintelligence/tests/__init__.py
@@ -0,0 +1,4 @@
+from .alien_vault_test import AlienVaultTestCase
+from .pulsedive_test import PulsediveTestCase
+from .tasks_test import TestTasks
+from .virus_total_test import VirusTotalTestCase
