0a164e18e3a2af4187f868293f2b3cb71e42f55d
MaciaKing <maciasalvasalva@gmail.com>
2024-05-03 16:41:55 +0200

    [fix]: change primary key If the domain is the primary key, then if the same data is extracted from other font, will lose information

--- cyberintelligence/models/data_extracted.py
+++ cyberintelligence/models/data_extracted.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class DataExtracted(models.Model):
     extracted_from = models.CharField(max_length=30, default='')
-    domain = models.CharField(max_length=None, default='', primary_key=True)
+    domain = models.CharField(max_length=None, default='')
     data_extracted = models.JSONField(default=dict) 
     white_list = models.BooleanField()
